831c57ed84c6c9e8abb80bb05cd4840e57d858ca
EXcomun1cado <s.ismoil0818@gmail.com>
2024-08-17 22:37:49 +0300

    Update module_3_4.py

--- module_3_4.py
+++ module_3_4.py
@@ -4,7 +4,7 @@ def single_root_words(root_word, *other_words):
 
     same_words = []
     for words in other:
-        if word in words:
+        if word in words or words in word:
             same_words.append(words)
 
     return same_words
